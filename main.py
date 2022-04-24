3bc970b86eb9ff0753043b84bbc8fade601f453e
ARYAN NAVEEN <aryan_naveen@college.harvard.edu>
2022-04-23 20:00:11 -0400

    face detection

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('frontalface.xml')
+def main():
+    face_cascade = cv2.CascadeClassifier('frontalface.xml')
 
-vid = cv2.VideoCapture(0)
+    vid = cv2.VideoCapture(0)
 
 
-if not vid:
-    print("!!! Failed VideoCapture: invalid parameter!")
+    if not vid:
+        print("!!! Failed VideoCapture: invalid parameter!")
 
-while True:
-    # Read the frame
-    _, img = vid.read()
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Display
-    cv2.imshow('img', img)
-    # Stop if escape key is pressed
-    k = cv2.waitKey(30) & 0xff
-    if k==27:
-        break
-# Release the VideoCapture object
-cap.release()
+
+    faceConfidence  = 0
+    while True:
+        # Read the frame
+        _, img = vid.read()
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        if len(faces) >= 1:
+            faceConfidence += 0.05
+        else:
+            faceConfidence = 0
+        
+        if faceConfidence > 0.8:
+            print("FACE DETECTED!!!!!")
+        else:
+            print()
+
+
+    # Release the VideoCapture object
+    cap.release()
+
+
+main()
